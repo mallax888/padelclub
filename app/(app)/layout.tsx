c65ff92c4fa9446421aa9b6a24e15e093de9e128
Malcolm Moffett <MMoffett@tonkintaylor.co.nz>
2026-06-09 13:26:12 +1200

    full colour overhaul silver background

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -15,7 +15,7 @@ export default async function AppLayout({
   }
 
   return (
-    <div className="min-h-screen bg-[#C0C0C0]">
+    <div className="min-h-screen bg-[#D8D8D8]">
       <Navbar />
       <main className="max-w-6xl mx-auto px-3 sm:px-4 py-6 overflow-x-hidden">
         {children}
